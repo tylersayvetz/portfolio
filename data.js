ac0d67f344eb4b2a4811361072291f95be3be0a9
Tyler <tylersayvetz@gmail.com>
2020-05-26 13:47:47 -0700

    remove empty data

--- data.js
+++ data.js
@@ -4,15 +4,5 @@ const data = [
     "content": "CLIJ is a secure, cloud-based note-taking app targetting GUI and CLI-oriented users.",
     "image": "./images/CLIJ.png",
     "link": "https://cli-journal.web.app/"
-  },
-  {
-    "content": "some data",
-    "image": "#",
-    "link": "#"
-  },
-  {
-    "content": "a blurb or two",
-    "image": "#",
-    "link": "#"
   }
 ];
