d29b6e86375e95eb06c25ac8a840f732100d6386
Tyler <tylersayvetz@gmail.com>
2020-05-26 16:10:28 -0700

    drawer styling update, add links  to gh

--- assets/js/getJson.js
+++ assets/js/getJson.js
@@ -1,27 +1,28 @@
-
 const contentArea = document.getElementById('_contentContainer');
 
 data.forEach((entry, i) => {
-  const newDiv = document.createElement('div')
-  newDiv.setAttribute('class', 'container content')
-  newDiv.setAttribute('id', i)
-  newDiv.setAttribute('style', `background-image: url('${entry.image}')`)
-  addDrawer(entry, newDiv, i)
-  contentArea.append(newDiv);
-})
+    const newDiv = document.createElement('div');
+    newDiv.setAttribute('class', 'container content');
+    newDiv.setAttribute('id', i);
+    newDiv.setAttribute('style', `background-image: url('${entry.image}')`);
+    addDrawer(entry, newDiv, i);
+    contentArea.append(newDiv);
+});
 
 function addDrawer(data, parent, i) {
-  const drawer = document.createElement('div')
-  drawer.setAttribute('class', 'drawer')
-  drawer.innerHTML = makeContent(data)
-  parent.append(drawer)
+    const drawer = document.createElement('div');
+    drawer.setAttribute('class', 'drawer');
+    drawer.innerHTML = makeContent(data);
+    parent.append(drawer);
 }
 
 function makeContent(data) {
-  return (
-    `<p>${data.content}</p>
-    <a href="${data.link}">See More</a>
-    
-    `
-  )
+    return (
+        `
+            <h2>${data.title}</h2>
+            <p>${data.content}</p>
+            <a href="${data.gh}">Project Repository</a>
+            <a href="${data.link}">Deployment</a>
+        `
+    );
 }
--- data.js
+++ data.js
@@ -4,18 +4,21 @@ const data = [
     "title": "CLIJ",
     "content": "CLIJ is a secure, cloud-based note-taking app targetting GUI and CLI-oriented users.",
     "image": "./images/clij-banner-2x.png",
-    "link": "https://cli-journal.web.app/"
+    "link": "https://cli-journal.web.app/",
+    "gh": "https://github.com/Coders-Living-Instant-Work-Journal"
   },
   {
     "title": "Battlefleet",
     "content": "Battlefleet is a server-rendered two-player battleship game.",
     "image": "./images/battlefleet-banner-2x.png",
-    "link": "http://battlefleet.tylersayvetz.com"
+    "link": "http://battlefleet.tylersayvetz.com",
+    "gh": "https://github.com/pesto-pasta/battle-fleet-online"
   },
   {
     "title": "2048",
     "content": "A remake of the classic, exponentially additictive time-killer, 2048",
     "image": "./images/2048-banner-2x.png",
-    "link": "https://tylers2048replica.herokuapp.com/"
+    "link": "https://tylers2048replica.herokuapp.com/",
+    "gh": "https://github.com/tylersayvetz/2048"
   },
 ];
