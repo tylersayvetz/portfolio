d00a30a3418d4f5dbd3a29d303bcfd4d090d418a
Tyler <tylersayvetz@gmail.com>
2020-05-26 15:08:52 -0700

    debug, style, add battlefleet

--- data.js
+++ data.js
@@ -1,8 +1,15 @@
 
 const data = [
   {
+    "title": "CLIJ",
     "content": "CLIJ is a secure, cloud-based note-taking app targetting GUI and CLI-oriented users.",
-    "image": "./images/CLIJ.png",
+    "image": "./images/clij-banner-2x.png",
     "link": "https://cli-journal.web.app/"
-  }
+  },
+  {
+    "title": "Battlefleet",
+    "content": "Battlefleet is a server-rendered two-player battleship game.",
+    "image": "./images/battlefleet-banner-2x.png",
+    "link": "http://battlefleet.tylersayvetz.com"
+  },
 ];
