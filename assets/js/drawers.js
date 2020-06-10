203f58e8292fcb2c4c6b981d891a74af4e8dd58a
Tyler <tylersayvetz@gmail.com>
2020-06-10 10:43:19 -0700

    fix card click-through bug

--- assets/js/drawers.js
+++ assets/js/drawers.js
@@ -18,15 +18,15 @@ const elements = document.getElementsByClassName('content');
     })
 });
 
+
+//Collapse all drawers. Then slide out the selected drawer.
 function slideOut(e) {
-  //Collapse all drawers.
   const cards = document.getElementById('_contentContainer');
   const drawers = Array.from(cards.childNodes).map(node => node.firstChild)
   for (const drawer of drawers) {
     drawer.setAttribute('class', 'drawer')
   }
 
-  //Slide out the selected drawer
   e.target.firstChild.setAttribute('class', 'drawer-out');
 }
 
@@ -34,6 +34,20 @@ function slideIn(e) {
   e.target.firstChild.setAttribute('class', 'drawer');
 }
 
+//Do nothing when user taps/clicks on a link within the drawer. Otherwise toggle the drawer.
 function toggleSlide(e) {
+  if (eventExceptions(e)) return;
+
   return (e.target.firstChild.className === 'drawer-out') ?  slideIn(e) : slideOut(e);
+}
+
+//Determines if the passed event meets any of the defined criteria
+function eventExceptions(e) {
+  const Tags = {
+    A: 'A'
+  }
+  //Tag name exeptions
+  if (Tags[e.target.tagName]) return true
+
+  return false;
 }
