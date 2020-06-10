3386d732f0758672984c9f43625dff0a3c82d4f8
Tyler <tylersayvetz@gmail.com>
2020-06-10 11:38:10 -0700

    style cards

--- assets/js/drawers.js
+++ assets/js/drawers.js
@@ -9,45 +9,57 @@ const elements = document.getElementsByClassName('content');
     element.addEventListener('mouseleave', slideIn);
 
     //At touch start, set movingTouch to false. If the user triggers a move event at any time, set movingTouch to true.
-    element.addEventListener('touchstart', () => { movingTouch = false });
-    element.addEventListener('touchmove', () => { movingTouch = true })
+    element.addEventListener('touchstart', () => {
+        movingTouch = false;
+    });
+    element.addEventListener('touchmove', () => {
+        movingTouch = true;
+    });
 
     //At the end of a touch, determine if it was a click or a move.
     element.addEventListener('touchend', (e) => {
-      if (movingTouch === false) toggleSlide(e)
-    })
+        if (movingTouch === false) toggleSlide(e);
+    });
 });
-
 
 //Collapse all drawers. Then slide out the selected drawer.
 function slideOut(e) {
-  const cards = document.getElementById('_contentContainer');
-  const drawers = Array.from(cards.childNodes).map(node => node.firstChild)
-  for (const drawer of drawers) {
-    drawer.setAttribute('class', 'drawer')
-  }
+    const container = document.getElementById('_contentContainer');
+    const cards = Array.from(container.childNodes).filter((card) => card !== e.target);
+    const drawers = cards.map((card) => card.firstChild);
 
-  e.target.firstChild.setAttribute('class', 'drawer-out');
+    //Collapse and add filtering
+    for (const card of cards) {
+        card.setAttribute('class', 'container content darkened');
+        card.firstChild.setAttribute('class', 'drawer');
+    }
+    e.target.setAttribute('class', 'container content')
+    e.target.firstChild.setAttribute('class', 'drawer-out');
 }
 
 function slideIn(e) {
-  e.target.firstChild.setAttribute('class', 'drawer');
+    const container = document.getElementById('_contentContainer');
+    for (const card of Array.from(container.childNodes)) {
+        card.setAttribute('class', 'container content');
+    }
+
+    e.target.firstChild.setAttribute('class', 'drawer');
 }
 
 //Do nothing when user taps/clicks on a link within the drawer. Otherwise toggle the drawer.
 function toggleSlide(e) {
-  if (eventExceptions(e)) return;
+    if (eventExceptions(e)) return;
 
-  return (e.target.firstChild.className === 'drawer-out') ?  slideIn(e) : slideOut(e);
+    return e.target.firstChild.className === 'drawer-out' ? slideIn(e) : slideOut(e);
 }
 
 //Determines if the passed event meets any of the defined criteria
 function eventExceptions(e) {
-  const Tags = {
-    A: 'A'
-  }
-  //Tag name exeptions
-  if (Tags[e.target.tagName]) return true
+    const Tags = {
+        A: 'A'
+    };
+    //Tag name exeptions
+    if (Tags[e.target.tagName]) return true;
 
-  return false;
+    return false;
 }
